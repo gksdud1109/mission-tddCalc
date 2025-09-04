2ec3f3d55b628dd87b423648aa2569fce65e9f76
gksdud1109 <vectorh532@gmail.com>
2025-09-04 21:13:54 +0900

    step9-blue

--- tddCalc/src/main/java/Calc.java
+++ tddCalc/src/main/java/Calc.java
@@ -1,46 +1,56 @@
 import java.util.*;
 
 public class Calc {
-    public static int run(String args){
 
-        String[] tokens = args.split("\\s+", 2); // 첫공백으로 왼쪽 오른쪽으로 나눔
+    private static int idx;
+    private static String ParseRest;
 
-        if (tokens.length == 0 || tokens[0].isEmpty()) {
-            return 0;
+    public static int run(String args) {
+        ParseRest = args;
+        idx = 0;
+        int val = parseExpr();
+        skipSpaces();
+        return val;
+    }
+
+    private static int parseExpr(){
+        int val = parseNumber();
+        while(true){
+            skipSpaces();
+            if (match('+')) {
+                val += parseNumber();
+            }
+            else if (match('-')) {
+                val -= parseNumber();
+            }
+            else break;
         }
+        return val;
+    }
 
-        String first = tokens[0];
-        String rest = (tokens.length>1) ? tokens[1] : "";   // tokens[1]에 아무것도 없을경우 빈문자열 넣어줌
-
-        try{
-            int left = Integer.parseInt(first); // 좌항 int로 파싱 시도
-            if (rest.isEmpty()) {
-                return left;
-            }
-            String[] operator = rest.split("\\s+", 2);
-            if(operator.length == 0){
-                return left;
-            }
-            String op = operator[0];
-            String right = (operator.length>1) ? operator[1] : "";
-
-            switch (op) {
-                case "+": return left + run(right);
-                case "-": return left - run(right);
-                case "*": return left * run(right);
-                case "/": return left / run(right);
-                default:
-                    throw new NumberFormatException("지원하지 않는 연산자: " + first);
-            }
-        } catch(NumberFormatException e){
-            if(first.equals("+"))   // tokens[0]이 숫자가 아니라면 연산자임 나머지 우항 리턴
-                return run(rest);
-            else if(first.equals("-")) {
-                return -run(rest);
-            }
-
-            throw new NumberFormatException("지원하지 않는 연산자: " + first);
+    private static int parseNumber(){
+        skipSpaces();
+        if(idx >= ParseRest.length() || !Character.isDigit(ParseRest.charAt(idx))){
+            throw new IllegalArgumentException("Invalid number");
         }
+        int num = 0;
+        while(idx < ParseRest.length() && Character.isDigit(ParseRest.charAt(idx))){
+            num = num*10 + (ParseRest.charAt(idx) - '0');
+            idx++;
+        }
+        return num;
+    }
+
+    private static boolean match(char c){
+        skipSpaces();
+        if(idx < ParseRest.length() && ParseRest.charAt(idx) == c){
+            idx++;
+            return true;
+        }
+        return false;
+    }
+    private static void skipSpaces(){
+        while(idx < ParseRest.length() && Character.isWhitespace(ParseRest.charAt(idx))) idx++;
     }
 
 }
