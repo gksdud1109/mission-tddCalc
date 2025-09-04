e300ab26d340c6597a99362e7b9e0d9067df9753
gksdud1109 <vectorh532@gmail.com>
2025-09-04 21:39:28 +0900

    숙제 끝~

--- tddCalc/src/main/java/Calc.java
+++ tddCalc/src/main/java/Calc.java
@@ -27,28 +27,39 @@ public class Calc {
     }
 
     private static int parseTerm(){
-        int Tval = parseNumber();
+        int Tval = parseFactor();
         while(true){
             skipSpaces();
             if(match('*')){
-                Tval *= parseNumber();
+                Tval *= parseFactor();
             } else if(match('/')){
-                Tval /= parseNumber();
+                Tval /= parseFactor();
             }
             else break;
         }
         return Tval;
     }
 
+    private static int parseFactor(){
+        skipSpaces();
+        if(match('+')) return parseFactor();
+        if(match('-')) return -parseFactor();
+
+        if (match('(')) {
+            int Fval = parseExpr();
+            skipSpaces();
+            if(!match(')')){
+                throw new IllegalArgumentException("')' expected at pos " + idx);
+            }
+            return Fval;
+        }
+        return parseNumber();
+    }
+
     private static int parseNumber(){
         skipSpaces();
-        if(idx >= ParseRest.length()){
-            throw new IllegalArgumentException("Invalid number");
-        }
-        else if(!Character.isDigit(ParseRest.charAt(idx))){
-            if(match('-')) return -parseNumber();
-            else
-                throw new IllegalArgumentException("Invalid number");
+        if (idx >= ParseRest.length() || !Character.isDigit(ParseRest.charAt(idx))) {
+            throw new IllegalArgumentException("Number expected at pos " + idx);
         }
         int num = 0;
         while(idx < ParseRest.length() && Character.isDigit(ParseRest.charAt(idx))){
