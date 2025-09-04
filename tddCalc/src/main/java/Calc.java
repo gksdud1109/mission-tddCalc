4c2aa3c11ae9d122174d8e9523f231e4fbdb133a
gksdud1109 <vectorh532@gmail.com>
2025-09-04 20:41:25 +0900

    step8-blue

--- tddCalc/src/main/java/Calc.java
+++ tddCalc/src/main/java/Calc.java
@@ -14,12 +14,17 @@ public class Calc {
 
         try{
             int left = Integer.parseInt(first); // 좌항 int로 파싱 시도
+
             return left + run(rest);    //되면 (좌항 + run(rest)리턴값) 리턴
         } catch(NumberFormatException e){
             if(first.equals("+"))   // tokens[0]이 숫자가 아니라면 연산자임 나머지 우항 리턴
                 return run(rest);
+            else if(first.equals("-")) {
+                return -run(rest);
+            }
 
             throw new NumberFormatException("지원하지 않는 연산자: " + first);
         }
     }
+
 }
