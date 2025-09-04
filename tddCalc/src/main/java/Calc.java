dff85e62bfd363abdeb7cc168c0d637ca2e0c09c
gksdud1109 <vectorh532@gmail.com>
2025-09-04 20:56:43 +0900

    step9-red

--- tddCalc/src/main/java/Calc.java
+++ tddCalc/src/main/java/Calc.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Calc {
     public static int run(String args){
 
-        String[] tokens = args.split("\\s", 2); // 첫공백으로 왼쪽 오른쪽으로 나눔
+        String[] tokens = args.split("\\s+", 2); // 첫공백으로 왼쪽 오른쪽으로 나눔
 
         if (tokens.length == 0 || tokens[0].isEmpty()) {
             return 0;
@@ -14,8 +14,24 @@ public class Calc {
 
         try{
             int left = Integer.parseInt(first); // 좌항 int로 파싱 시도
+            if (rest.isEmpty()) {
+                return left;
+            }
+            String[] operator = rest.split("\\s+", 2);
+            if(operator.length == 0){
+                return left;
+            }
+            String op = operator[0];
+            String right = (operator.length>1) ? operator[1] : "";
 
-            return left + run(rest);    //되면 (좌항 + run(rest)리턴값) 리턴
+            switch (op) {
+                case "+": return left + run(right);
+                case "-": return left - run(right);
+                case "*": return left * run(right);
+                case "/": return left / run(right);
+                default:
+                    throw new NumberFormatException("지원하지 않는 연산자: " + first);
+            }
         } catch(NumberFormatException e){
             if(first.equals("+"))   // tokens[0]이 숫자가 아니라면 연산자임 나머지 우항 리턴
                 return run(rest);
