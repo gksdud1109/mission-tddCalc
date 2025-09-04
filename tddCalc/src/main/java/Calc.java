689bd76186b90646eb8365a51610145cdc1a35ee
gksdud1109 <vectorh532@gmail.com>
2025-09-04 17:46:54 +0900

    step1-blue

--- tddCalc/src/main/java/Calc.java
+++ tddCalc/src/main/java/Calc.java
@@ -1,6 +1,25 @@
+import java.util.*;
 
 public class Calc {
     public static int run(String args){
-        return 2; //꼼수 구현
+
+        String[] tokens = args.split("\\s", 2); // 첫공백으로 왼쪽 오른쪽으로 나눔
+
+        if (tokens.length == 0 || tokens[0].isEmpty()) {
+            return 0;
+        }
+
+        String first = tokens[0];
+        String rest = (tokens.length>1) ? tokens[1] : "";   // tokens[1]에 아무것도 없을경우 빈문자열 넣어줌
+
+        try{
+            int left = Integer.parseInt(first); // 좌항 int로 파싱 시도
+            return left + run(rest);    //되면 (좌항 + run(rest)리턴값) 리턴
+        } catch(NumberFormatException e){
+            if(first.equals("+"))   // tokens[0]이 숫자가 아니라면 연산자임 나머지 우항 리턴
+                return run(rest);
+
+            throw new NumberFormatException("지원하지 않는 연산자: " + first);
+        }
     }
 }
